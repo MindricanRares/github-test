bbe207706b38dfa00d702ef7c7e6fce62513bfe5
Rares Mindrican <RM055204@cerner.net>
2018-08-02 19:52:03 +0300

    - test

--- branch.js
+++ branch.js
@@ -1,6 +1,6 @@
 const nodeCmd = require('node-cmd');
 nodeCmd.get('git rev-parse --abbrev-ref HEAD', (err, branchName, stderr) => {
-
+    console.log("Wohooooo");
     nodeCmd.get(`heroku create github-workshop-${branchName}`);
     nodeCmd.get(`heroku git:remote -a github-workshop-${branchName}`); 
     nodeCmd.get(`git push heroku master`); 

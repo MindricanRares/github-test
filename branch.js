e64b50c11b6425d59455e057e5c2579a3fc5c63b
Rares Mindrican <RM055204@cerner.net>
2018-08-02 19:49:44 +0300

    - fix heroku autodeploy

--- branch.js
+++ branch.js
@@ -1,8 +1,8 @@
 const nodeCmd = require('node-cmd');
 nodeCmd.get('git rev-parse --abbrev-ref HEAD', (err, branchName, stderr) => {
 
-    nodeCmd.get('')
     nodeCmd.get(`heroku create github-workshop-${branchName}`);
-    nodeCmd.get(`git push heroku github-workshop-${branchName}:master`); 
+    nodeCmd.get(`heroku git:remote -a github-workshop-${branchName}`); 
+    nodeCmd.get(`git push heroku master`); 
 
 });

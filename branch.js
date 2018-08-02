80fabd2566c6ed43312e6d67b0b07b581f9b208b
Rares Mindrican <RM055204@cerner.net>
2018-08-02 20:15:58 +0300

    still working on heroku deloy

--- branch.js
+++ branch.js
@@ -1,8 +1,4 @@
 const nodeCmd = require('node-cmd');
 nodeCmd.get('git rev-parse --abbrev-ref HEAD', (err, branchName, stderr) => {
-
-    nodeCmd.get('')
     nodeCmd.get(`heroku create github-workshop-${branchName}`);
-    nodeCmd.get(`git push heroku github-workshop-${branchName}:master`); 
-
 });
